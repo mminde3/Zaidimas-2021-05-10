c99937ae2af65bb59ea31aea9d19a480a0e7251b
Mindaugas <mminde3@gmail.com>
2021-05-11 23:12:04 +0300

    Panaikinau klaidas
    Buvo klaidu panaikinau

--- script.js
+++ script.js
@@ -1,34 +1,66 @@
 document.addEventListener('click', function(event){
     var target = event.target
-    console.log(target);
     markX(target);
 }); 
 
 function markX(element){
     if (element.classList.contains("cell") && element.innerText == ""){
         element.innerText = "X";
-        opponentMove();
+
+        let gameContinues = checkResult();
+        if (gameContinues){
+            opponentMove();
+            checkResult();
+        }
     }
 
 }
 
 function opponentMove(){
 
-    let elements = Array.from(document.querySelectorAll(".cell"));
-    elements = elements.filter(function(el){
-        return el.innerText == ""
-    });
+    var cells = Array.from(document.getElementsByClassName("cell"));
+    cells = cells.filter(cell => cell.innerText == "");
 
-    let randomNo = randomInteger(0, elements.length);
+    let randomNumber = randomInteger(0, cells.length);
 
-    let selectedElement = elements[randomNo];
+    let randomCell = cells[randomNumber];
 
-    selectedElement.innerText = "0";
-
+    randomCell.innerText = '0';
+    
 }
 
 function randomInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    const r = Math.random()*(max-min) + min;
+    return Math.floor(r);
   }
 
+function checkResult(){
+    WinnerDetermined();
+    return anyMovesLeft();
+}
 
+function anyMovesLeft(){
+    var cells = Array.from(document.getElementsByClassName("cell"));
+    cells = cells.filter(cell => cell.innerText == "");
+
+    if (cells.length == 0){
+        document.querySelector(".game--status").innerText = "DRAW";
+        return false; //Games finishes
+    }
+    return true; //Game continues
+}
+
+
+function WinnerDetermined(){
+    let winningCellCombinations = [
+        [0,1,3],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [3,5,7]
+    ]
+
+}
